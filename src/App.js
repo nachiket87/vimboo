4ada03614d7c175184951098cabb1a1406871dfc
Nachiket Pusalkar <nachiket87@gmail.com>
2021-01-21 22:32:35 -0500

    Add level 2

--- src/App.js
+++ src/App.js
@@ -4,23 +4,77 @@ import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/theme-gruvbox";
 import "ace-builds/src-noconflict/keybinding-vim";
-import { useState } from "react";
+import { useLayoutEffect, useState, useRef } from "react";
+
+const INSTRUCTIONS = [
+  ` A wild '%' is out of control! he keeps popping up everywhere on
+          this editor, remove him as fast as you can
+           Tip: there are several ways to navigate and delete a word with
+          vim. For this challenge, we recommend you stick to the basics. Use
+          'h' to move left, 'j' to move down, 'k' to move
+          up,'l' to move right
+          Once you've spotted the wild % put your cursor on it to aim your lazer
+          and shoot by pressing 'x'
+  `,
+  ` Time to get faster! 
+        You can type : followed by line number to take the cursor directly to the line you want to go to
+        You can also go to the end of each word by typing 'E' `,
+];
 
 const VALUES = [
-  `\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t%\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`,
-  `\n\n\n%\n\n\n\n\n\n\n\n\n\n\n`,
-  `\n%\n\n\n\n\n\n\n\n\n`,
-  `\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t%\n\n\n\n\n\n\n\n\n`,
-  `\n\n\n\nprint("%")\n\n\n\n\n\n\n\n\n`,
-  `\n\n\n\n\t\t\t\t\t\t\t\t\tCongratulations!\n\n\n\n`,
+  [
+    `\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t%\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`,
+    `\n\n\n%\n\n\n\n\n\n\n\n\n\n\n`,
+    `\n%\n\n\n\n\n\n\n\n\n`,
+    `\t\t\t\t\t\t%\n\n\n\n\n\n\n\n\n`,
+    `\n\n\n\nprint("%")\n\n\n\n\n\n\n\n\n`,
+    `\n\n\n\n\t\t\t\t\t\t\t\t\tCongratulations!\n\n\n\n`,
+  ],
+  [
+    `\n\n\n\n\nprint("%")\n\n\n\n\n\n\n\n\n`,
+    `\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t%\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`,
+    `\n%\n\n\n\n\n\n\n\n\n`,
+    `\n\n\n%\n\n\n\n\n\n\n\n\n\n\n`,
+    `\n\n\n\t\t\t\t\t\t\t\t\t%\n\n\n\n\n\n\n\n\n`,
+    `\n\n\n\nprint("%")\n\n\n\n\n\n\n\n\n`,
+  ],
 ];
 
 function App() {
   const [score, setScore] = useState(0);
+  const [startTimer, setTimer] = useState(false);
   const [timeLeft, setTimeLeft] = useState(30);
   const [gameOver, setGameOver] = useState(false);
+  const [currentLevel, setCurrentLevel] = useState(0);
+  const [size, setSize] = useState([0, 0]);
+  const editor = useRef(null);
 
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      setSize([window.innerWidth - 50, window.innerHeight - 50]);
+    };
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  const reset = () => {
+    setScore(0);
+    setGameOver(false);
+    setTimeLeft(30);
+    editor.current.editor.textInput.focus();
+    console.log(editor.current.editor.session);
+  };
+  const nextLesson = () => {
+    setTimer(false);
+    setScore(0);
+    setGameOver(false);
+    setCurrentLevel(currentLevel + 1);
+    setTimeLeft(20);
+    editor.current.editor.textInput.focus();
+  };
   const change = (event, value) => {
+    console.log(event);
     if (value.action === "remove" && value.lines.includes("%")) {
       setScore(score + 1);
       if (score + 1 === 5) {
@@ -33,51 +87,38 @@ function App() {
       }
     }
   };
-  const itLoaded = (editor) => {
-    editor.focus();
-    editor.gotoLine(0, 0);
-  };
+
   return (
     <div className="App">
-      <Timer
-        timeLeft={timeLeft}
-        setTimeLeft={setTimeLeft}
-        gameOver={gameOver}
-      />
-      <div>
-        <p>
-          A wild <b>'%'</b> is out of control! he keeps popping up everywhere on
-          this editor, remove him as fast as you can
-        </p>
-        <p>
-          <b> Tip: </b>there are several ways to navigate and delete a word with
-          vim. For this challenge, we recommend you stick to the basics. Use{" "}
-          <b>'h'</b> to move left, <b>'j'</b> to move down, <b>'k'</b> to move
-          up,<b>'l'</b> to move right
-        </p>
-        <h3>
-          Once you've spotted the wild % put your cursor on it to aim your lazer
-          and shoot by pressing <b>'x'</b>
-        </h3>
-      </div>
+      <div>{INSTRUCTIONS[currentLevel]}</div>
       <div style={{ display: "grid", placeItems: "center" }}>
+        {startTimer ? (
+          <Timer
+            timeLeft={timeLeft}
+            setTimeLeft={setTimeLeft}
+            gameOver={gameOver}
+          />
+        ) : (
+          <></>
+        )}
+        <button onClick={() => reset()}> Reset </button>
+        <button onClick={() => nextLesson()} display={gameOver}>
+          Next Lesson
+        </button>
+        <button onClick={() => setTimer(true)} display={gameOver}>
+          Start Timer
+        </button>
         <AceEditor
           theme="gruvbox"
           minLines={25}
-          value={VALUES[score]}
+          ref={editor}
+          value={VALUES[currentLevel][score]}
           onChange={(e, v) => change(e, v)}
-          height="800px"
-          onLoad={itLoaded}
-          width={
-            window.innerWidth < 400 ? window.innerWidth : window.innerWidth - 50
-          }
+          focus={true}
+          onLoad={(editor) => editor.gotoLine(0, 0)}
+          width={size[0]}
           keyboardHandler="vim"
           name="vimeditor"
-          setOptions={{
-            autoScrollEditorIntoView: true,
-            scrollPastEnd: true,
-            tabSize: 4,
-          }}
         />
       </div>
     </div>
